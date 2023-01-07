556176a4a17dd2833d36b6364b96dbfdcc692103
IhorKasprov <kasprovigor@gmail.com>
2023-01-08 00:06:49 +0200

    add features

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Feature, Product
 
-# Register your models here.
+# Реєструємо ваші моделі.
+admin.site.register(User)
+admin.site.register(Feature)
+admin.site.register(Product)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Feature(models.Model):
+    feature = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f" {self.feature} "
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    imege = models.URLField()
+    category = models.ForeignKey(Feature, on_delete=models.PROTECT, related_name="category")
+
+    def __str__(self):
+        return f"{self.id}: {self.product_name} to {self.category}"
+
+    
