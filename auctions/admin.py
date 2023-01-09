7d255c6eb68b338b1b46d33e557cc8eae451cdc4
IhorKasprov <kasprovigor@gmail.com>
2023-01-09 16:54:09 +0200

    add class product

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import User, Feature, Product
+from .models import User, Feature, Product, Profile
+
 
 # Реєструємо ваші моделі.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'firstname', 'lastname']
+
 admin.site.register(User)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Feature)
 admin.site.register(Product)
--- auctions/models.py
+++ auctions/models.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class User(AbstractUser):
     pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user}"
+    
 
 class Feature(models.Model):
     feature = models.CharField(max_length=64)
@@ -11,13 +22,18 @@ class Feature(models.Model):
     def __str__(self):
         return f" {self.feature} "
 
+
 class Product(models.Model):
     product_name = models.CharField(max_length=64)
     description = models.CharField(max_length=128)
     imege = models.URLField()
     category = models.ForeignKey(Feature, on_delete=models.PROTECT, related_name="category")
+    marketeer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="marketeer")
 
     def __str__(self):
-        return f"{self.id}: {self.product_name} to {self.category}"
+        return f"{self.id}: {self.product_name}, {self.category}, {self.marketeer}"
+
+
+
 
     
